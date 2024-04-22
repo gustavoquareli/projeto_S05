742e789e5b0f33afa366d1ac29a62ee697634320
Gustavo Quareli <gustavo.quareli@gmail.com>
2024-04-22 20:34:20 -0300

    Update requirements.txt and app.py

--- SRC/app.py
+++ SRC/app.py
@@ -1,8 +1,12 @@
 import streamlit as st
 import pandas as pd
-import ploty_express as px
+import plotly.express as px
 
-cad_data = pd.read_csv('vehicles.csv')  # lendo os dados
+car_data = pd.read_csv('data/vehicles.csv')  # lendo os dados
+
+car_data.dropna(subset=['odometer'], inplace=True)  # apagando valores nulos
+
+
 hist_button = st.button('Criar um histograma')  # criar um botão
 
 if hist_button:  # se o botão for clicado
@@ -11,7 +15,17 @@ if hist_button:  # se o botão for clicado
         'Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
 
     # criar um histograma
-    fig = px.histogram(car_data, x="odometer")
+    fig = px.histogram(car_data,
+                       x="odometer",
+                       title="Estatística de Vendas de Carros por Quilometragem")
 
     # exibir um gráfico Plotly interativo
     st.plotly_chart(fig, use_container_width=True)
+
+    # criar um gráfico de dispersão
+    fig2 = px.scatter(car_data,
+                      x="odometer",
+                      y="price",
+                      title="Comparando o preço de venda do veículo com a quilometragem")
+
+    st.plotly_chart(fig2, use_container_width=True)  # exibindo
